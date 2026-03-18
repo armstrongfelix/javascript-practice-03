d2c09c2b77e534843ea183f312b9ff2858079553
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-18 21:29:20 +0100

    project initialzed with details.jsfile

---
+++ details.js
@@ -0,0 +1,8 @@
+const firstName = "Armstrong";
+const lastName = "Felix";
+const country = "Nigeria";
+const hubby = "Coding";
+const age = 22;
+console.log(
+  `My Full Name is ${firstName}  ${lastName}. \nI am ${age} old and i live in ${country}. \nIn my free time, I Enjoy ${hubby}`,
+);
