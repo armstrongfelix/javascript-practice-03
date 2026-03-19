a87f60660881329c1e6620878961eca44dac6f41
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-19 10:11:44 +0100

    added largest-sum.js File

---
+++ largest-num.js
@@ -0,0 +1,10 @@
+let a = 42;
+let b = 78;
+let c = 55;
+if (a > b && a > c) {
+  console.log(`The largest number is ${a}.`);
+} else if (b > a && b > c) {
+  console.log(`$The largest number is ${b}.`);
+} else if (c > a && c > b) {
+  console.log(`The largest number is ${c}.`);
+}
