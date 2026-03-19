56470e51d9df2ca3a56ccb56fbfa0ce26883c289
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-19 16:28:06 +0100

    added number-checker.js, print-even-number.js, string-checker.js files

---
+++ string-checker.js
@@ -0,0 +1,7 @@
+const sentence = "JavaScript is fun";
+const sentenceLength = sentence.length;
+const sentenceUpper = sentence.toUpperCase();
+const sentenceLower = sentence.toLowerCase();
+console.log(
+  `Length: ${sentenceLength} \nUppercase: ${sentenceUpper} \nLowercase: ${sentenceLower} \nSentence Includes Fun: ${sentence.includes("fun")}`,
+);
