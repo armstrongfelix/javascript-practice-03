7a0677d8655a4ccae2a8c52f01b5a4b3139c299f
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-19 16:37:37 +0100

    moddified number-chech logics

---
+++ number-checker.js
@@ -0,0 +1,9 @@
+const value = -15;
+
+if (value > 0) {
+  console.log(`${value} is a positive number.`);
+} else if (value < 0) {
+  console.log(`${value} is a negative number.`);
+} else {
+  console.log(`${value} is zero.`);
+}
